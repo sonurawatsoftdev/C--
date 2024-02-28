3932208d4e6701d070636e7a22050fcf8efc3985
sonurawatsoftdev <sonurawatsoftdev@gmail.com>
2024-02-28 10:01:18 +0530

    C

---
+++ Ass2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+  int main(){
+    int a = 5, b = 15, sum;
+            sum=a+b;
+               std:: cout << sum;
+}
---
+++ Ass3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+  int main(){
+    int a = 15, b = 3, sum;
+            sum=a/b;
+               std:: cout << sum;
+}
